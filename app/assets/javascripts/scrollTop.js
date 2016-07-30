bee31a4a09d8647bd1763fe9ad366211e5d33a6f
stevenchentw <sadz9999@hotmail.com>
2016-07-30 15:53:13 +0800

    finish

---
+++ app/assets/javascripts/scrollTop.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('body').on('click', '.page-scroll a', function(event) { var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 800, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
---
+++ app/controllers/concerns/product_controller.rb
@@ -0,0 +1,11 @@
+class ProductController < ApplicationController
+
+def square_1
+
+end
+
+def triangle_1
+
+end
+
+end
